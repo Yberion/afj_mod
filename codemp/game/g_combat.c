2c1325c521a1bf4602bba8a2278439dc1b36d881
Brandon <brandon.largeau@gmail.com>
2017-02-25 17:18:43 +0100

    Remove chat invulnerability in duel

--- codemp/game/g_combat.c
+++ codemp/game/g_combat.c
@@ -4442,7 +4442,7 @@ void G_Damage( gentity_t *targ, gentity_t *inflictor, gentity_t *attacker, vec3_
 	if (!targ)
 		return;
 
-	if (targ->client && (targ->client->ps.eFlags & EF_TALK))
+	if (targ->client && (targ->client->ps.eFlags & EF_TALK) && targ->client->ps.duelInProgress == qfalse)
 		return;
 
 	if (targ->client && (targ->client->pers.afjUser.isSlept))
